417c62b7d3c6b86f13414521b436b81edaeb52be
Egor <egormakar98@mail.ru>
2018-06-11 13:00:33 +0300

    Enemy random skin from resources

--- RPG_Sharp/EntityEnemy.cs
+++ RPG_Sharp/EntityEnemy.cs
@@ -3,32 +3,41 @@ using System.Drawing;
 
 namespace RPG_Sharp
 {
+    /// <summary>
+    /// Враг - злодей - враждебный персонаж (ИИ)
+    /// </summary>
     class EntityEnemy : EntityAlive
     {
 
         static Random r = new Random();
         public override void SetImage()
         {
-            int p = r.Next(1,5);
-        
-        switch (p)
-    	{
-		    case 1:
-                this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy.png");
-                break;
-            case 2:
-                this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy2.png");
-                break;
-            case 3:
-                this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy3.png");
-                break;
-            case 4:
-                this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy4.png");
-                break;
-            default:
-            break;
+            // Число различных скинов мобов (от 0 до enemyTypes-1)
+            const int enemyTypes = 5;
+            int p = r.Next(0,enemyTypes);
 
-	    }
+            this.PictureBox.Image = Properties.Resources.ResourceManager.GetObject("Enemy" + p) as Image;
+            /*
+            // Определение скина моба раньше 
+            switch (p)
+    	    {
+		        case 1:
+                    this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy.png");
+                    break;
+                case 2:
+                    this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy2.png");
+                    break;
+                case 3:
+                    this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy3.png");
+                    break;
+                case 4:
+                    this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy4.png");
+                    break;
+                default:
+                break;
+            }
+            */
+
             //this.PictureBox.Image = Image.FromFile(Environment.CurrentDirectory + @"\Enemy.png");
             base.SetImage();
         }
